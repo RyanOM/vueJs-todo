5a0bcbe5cf5a56c2deb6ee8b1b7fc26cf992c0bf
Ryan <omullan.ryan@gmail.com>
2018-05-13 11:21:21 -0300

    add data to our todolist

--- vueJs-todo/src/App.vue
+++ vueJs-todo/src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TodoList
+  },
+
+  // data to template
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }]
+    }
+  }
 }
 </script>
 
