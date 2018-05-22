edb1cfe1dd0d6e8489a09c3ef09c36bd486fd04f
Ryan <omullan.ryan@gmail.com>
2018-05-21 21:36:11 -0300

    add delete todo button

--- vueJs-todo/src/components/TodoList.vue
+++ vueJs-todo/src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div class="">
     <p>Completed Tasks: {{todos.filter(todo=> {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo=> {return todo.done === false}).length}}</p>
-    <todo v-for="todo in todos" v-bind:todo="todo" :key="todo.id"></todo>
+    <todo v-for="todo in todos" v-bind:todo="todo" :key="todo.id" v-on:delete-todo="deleteTodo"></todo>
   </div>
 </template>
 
@@ -14,6 +14,12 @@ export default {
   props: ['todos'],
   components: {
     Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    }
   }
 }
 </script>
